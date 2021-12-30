a5ff1221a84daf096e2664a08a41d277fedf1594
Daisuke Sasaki <gerund25495641@gmail.com>
2021-12-31 08:32:02 +0900

    プロフィールページ作成のためにroutes.rbを編集

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # ここにpagesコントローラーのhomeアクションのルーティングを追加する
   root 'pages#home'
+
+  get '/users/:id', to: 'users#show', as 'user'
 end
