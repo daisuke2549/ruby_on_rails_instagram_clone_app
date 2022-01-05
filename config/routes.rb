a8f33f4a9e3e38f9e9ad6bb0fcaeeb5b7591b38d
Daisuke Sasaki <gerund25495641@gmail.com>
2022-01-05 09:13:09 +0900

    バリデーションの記述とルーティングをresourcesで一括設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # controllers: { registrations: 'registrations' } 
   root 'pages#home'
-  # get '/users/:id', to: 'users#show', as 'user'
+  resources :users, only: %i(show)
+  # resourcesで複数行のルーティング設定を一括設定可能
 end
