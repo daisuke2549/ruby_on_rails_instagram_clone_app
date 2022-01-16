ead189b5ad92fa36f8a69791fffaa4a7f61dffba
Daisuke Sasaki <gerund25495641@gmail.com>
2022-01-16 20:32:18 +0900

    ImageUploaderをインストール

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,6 @@ class Photo < ApplicationRecord
   belong_to:post
   
   validates :images,presence: true
+
+  mount_uploader :image, ImageUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   resources :users, only: %i(show)
   # resourcesで複数行のルーティング設定を一括設定可能
+
+  get '/posts/new', to:'posts#new'
+  post '/posts', to:'posts#create'
+  post '/posts/:post_id/photos', to: 'photos#create', as: 'post_photos'
 end
